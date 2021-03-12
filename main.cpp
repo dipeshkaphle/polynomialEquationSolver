430c299c452e4a0904acf74f926a0e54a1b9273e
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2021-03-12 09:21:50 +0545

    Removed unnecessary template usage
    The Equation class was templatized before but it was not at all needed.
    So I removed that and now it only keeps vector<double> to store the
    coefficients instead of a templatized vector

--- main.cpp
+++ main.cpp
@@ -6,10 +6,8 @@ using namespace std;
 int main() {
   cout << "Hello World!" << endl;
 
-  Equation eq(2);
+  Equation eq(3);
   eq.input();
-  eq.print_equation();
-  cout << endl;
   eq.print_equation();
   cout << endl;
   cout << endl;
@@ -18,14 +16,5 @@ int main() {
   for (auto x : ans)
     cout << x << " ";
   cout << endl;
-  // Equation eq2
-  // eq2.input();
-  // eq2.print_equation();
-  // get_equation(eq);
-  // std::vector<double> sol=find_roots(eq);
-  // for(int i=0;i<sol.size();i++)
-  //   {
-  //     std::cout<<sol[i]<<std::endl;
-  //   }
   return 0;
 }
